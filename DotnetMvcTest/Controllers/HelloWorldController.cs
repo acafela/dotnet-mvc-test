a88d9016d715562c583622c02895406dc9ffccbf
황윤성(yohwang) <yohwang@ebay.com>
2021-10-01 14:34:55 +0900

    ã프로젝트 초기화

---
+++ DotnetMvcTest/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace DotnetMvcTest.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 2)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
